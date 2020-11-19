fc190fe8d5e55d04effc499d1d645223609172d5
johnstewart0820 <john.s@outsource.codes>
2020-11-19 22:02:12 +0200

    Added unknown commit

--- backend/core/src/main/java/fr/be/your/self/model/UserConstants.java
+++
@@ -1,7 +0,0 @@
-package fr.be.your.self.model;
-
-public class UserConstants {
-	public static final String EDIT_PERMISSIONS_PATH = "/edit_permissions";
-	public static final String EDIT_ACCOUNT_TYPE_PATH = "/edit_account_type";
-	
-}
